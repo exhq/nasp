0b8f1574f50fd92c0ced434ae2de07183f666536
exhq <91651232+exhq@users.noreply.github.com>
2023-01-12 16:41:36 +0330

    AAAAAAAAAAAA

--- handling.ts
+++ handling.ts
@@ -2,15 +2,22 @@ import { CommandContext } from "@api/Commands";
 import { Message } from "discord.js";
 import { PartialDeep } from "type-fest";
 import { join } from 'path';
+import { profile } from "console";
+import { sendBotMessage } from '../../api/Commands/commandHelpers';
+import { send } from "process";
 
-export function handlecommands(ctx: CommandContext, message: string) {
+export async function handlecommands(ctx: CommandContext, message: string) {
+    let shit = message.split(" ").slice(1).toString();
 
     switch (message.split(" ")[0]) {
-        case "echo": Vencord.Webpack.findByProps("editMessage").sendMessage(ctx.channel.id, { content: message.split(" ").slice(1).toString() });
+        case "echo": Vencord.Webpack.findByProps("editMessage").sendMessage(ctx.channel.id, { content: shit });
+            return;
+        case "profile":
+            let lmao = await fetch(`https://dp.nea.moe/avatar/${shit.replaceAll("<@", "").replaceAll(">", "")}.json`);
+            let cbt = await lmao.json();
+            sendBotMessage(ctx.channel.id, { content: cbt.avatar });
+            return;
     }
 
 
-}
-function echo(channel, msg) {
-    Vencord.Webpack.findByProps("editMessage").sendMessage(channel, { content: msg });
 }
